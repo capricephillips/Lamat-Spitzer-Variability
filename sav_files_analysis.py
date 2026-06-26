778a32415154e7ba25c15d125b9364b3db3ebba1
Caprice Phillips <phillips.1622@buckeyemail.osu.edu>
2026-06-26 11:58:53 +0200

    Enhance plot_sav_files function with error handling
    Added error calculation and improved plot aesthetics.

--- sav_files_analysis.py
+++ sav_files_analysis.py
@@ -2,7 +2,14 @@ def plot_sav_files(file):
     sav_fname = file
     target_name = file.split('_calibch1')[0]
     sav_data = readsav(sav_fname)
-    fig, ax = plt.subplots(figsize=(10,6))
+    #getting error
+    binflux_error = np.std(sav_data['targfcal'] - np.roll(sav_data['targfcal'], 1))/np.sqrt(2)
+
+    binflux_error_array = binflux_error * (0*sav_data['bint'][:,0]+1)
+
+    fig, ax = plt.subplots(figsize=(12, 6.5))
+
+
     #Plotting part
     fig.patch.set_facecolor('white')
     ax.set_facecolor('white')
@@ -11,17 +18,20 @@ def plot_sav_files(file):
     ax.tick_params(axis='y', colors='black')  # Change y-axis tick label color
     ax.xaxis.label.set_color('black')         # Change x-axis label color
     ax.yaxis.label.set_color('black')         # Change y-axis label color
-    ax.title.set_color('white')
+    ax.title.set_color('black')
+    plt.title(f"{target_name}", fontsize=19)
     #plotting data binned
     plt.plot(sav_data['bint'][:,0], sav_data['targfcal'],linestyle='none',marker='.',label='Data',color='black')
     #plotting error for data
     plt.errorbar(sav_data['bint'][:,0], np.squeeze(sav_data['targfcal']),yerr=binflux_error_array,alpha=0.2,color='grey')
     #plotting best fit sin function
+
     plt.plot(sav_data['bint'][:,0], sav_data['mod1'],linestyle='-',label='Best fit sin',color='crimson',linewidth=3)
     plt.ylabel('Relative Flux',fontsize=19)
     plt.xlabel('Elapsed')
     plt.ylabel('Relative Flux',fontsize=19)
     plt.xlabel('Elapsed Time (hr)',fontsize=19)
+    plt.legend()
     plt.tight_layout()
     plt.savefig(f"{target_name}.pdf")
     
@@ -210,4 +220,4 @@ def stability_measure_sav_file(file, rotation_period):
     plt.show()
 
 #Testing script
-stability_measure_sav_file('2M0031+5749_calibch1_bin5_ap_opt.sav', rotation_period=1.64)
+#stability_measure_sav_file('2M0031+5749_calibch1_bin5_ap_opt.sav', rotation_period=1.64)
